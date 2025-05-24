3795471c6a1128726a8dcdd0cef49d74effe4724
Zuhair Ahmed <153817334+Zuhair0000@users.noreply.github.com>
2025-05-24 19:41:30 +0800

    NEW

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,7 @@ const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 3306, // <-- Add this line
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASS || "Zoherama1122",
   database: process.env.DB_NAME || "car_wash",
